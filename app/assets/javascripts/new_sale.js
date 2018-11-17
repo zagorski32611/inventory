7e09808f6839cada0447da36f4111514e7879b8b
Joe <j.j.zagorski@gmail.com>
2018-11-17 09:05:06 -0500

    added sale form stub

--- app/assets/javascripts/new_sale.js
+++ app/assets/javascripts/new_sale.js
@@ -1,19 +1,19 @@
 document.addEventListener("turbolinks:load", function() {
     
     const hideNewSale = document.querySelector("#sell_products").style.display = "none";
-    toggleShowProducts();
-    cancelShowProducts();
+    toggleShowSale();
+    cancelShowSale();
 });
 
-function toggleShowProducts() {
+function toggleShowSale() {
     document.querySelector("#sell_products_button").addEventListener("click", function(event){
         event.preventDefault();
         document.querySelector("#sell_products").style.display = "block";
     });
 }
 
-function cancelShowProducts() {
-    document.querySelector("#close_products_button").addEventListener("click", function(event) {
+function cancelShowSale() {
+    document.querySelector("#close_sale_button").addEventListener("click", function(event) {
         event.preventDefault();
         document.querySelector("#sell_products").style.display = "none";
     });
