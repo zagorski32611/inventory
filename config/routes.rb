c3e551d086b6e63ad411e86b83e61205fc7b5b69
Joe <j.j.zagorski@gmail.com>
2018-11-17 20:59:50 -0500

    added search route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  get :search, controller: :products
   root 'static_pages#index'
 
   resources :static_pages
